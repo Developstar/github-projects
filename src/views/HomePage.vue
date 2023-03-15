6532f2ce24e3ae802c2fe6b465330a962d529f58
Developstar <deyeye99@gmail.com>
2023-03-15 10:55:56 +0100

    created home page view

---
+++ src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="pageWrapper">
+      <div>
+        <h1>Home Page</h1>
+        <p>This is Home Page</p>
+      </div>
+    </section>
+  </template>
+  
+  <style>
+  .pageWrapper {
+    margin-top: 2rem;
+    text-align: center;
+  }
+  </style>
