58b0bc76d7c3aa9692c0d26276ad3437f272fd29
Developstar <deyeye99@gmail.com>
2023-03-15 10:56:45 +0100

    created single repo view

---
+++ src/views/SingleRepo.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="pageWrapper">
+      <div>
+        <h1>A single Repo</h1>
+        <p></p>
+      </div>
+    </section>
+  </template>
+  
+  <style>
+  .pageWrapper {
+    margin-top: 2rem;
+    text-align: center;
+  }
+  </style>
