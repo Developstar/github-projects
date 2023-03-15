70f6bcdc9242f5b92c2ada588183e7da0b9690fc
Developstar <deyeye99@gmail.com>
2023-03-15 10:55:04 +0100

    consumed vue-router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHistory} from "vue-router"
+import HomePage from './views/HomePage.vue'
+import MyRepos from './views/MyRepos.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [{
+        path: "/",
+        name: "HomePage",
+        component: HomePage
+    },
+    {
+        path: "/MyRepos",
+        name: "MyRepos",
+        component: MyRepos
+    }
+    
+]
+
+})
+
+
+createApp(App).use(router).mount('#app')
