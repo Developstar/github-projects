2a61d713e3a28cbab7d44788907147dddf6f42f5
Developstar <deyeye99@gmail.com>
2023-03-15 10:55:27 +0100

    created header navigation template

---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,19 @@
+<template>
+<nav class="nav_wrapper">
+    <div class="logo">
+      <router-link to="/">MyGit</router-link>
+    </div>
+    <div class="navigation">
+      <ul>
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+
+        <li>
+          <router-link to="/MyRepos">Repositories</router-link>
+        </li>
+      </ul>
+    </div>
+    </nav>
+</template>
+
