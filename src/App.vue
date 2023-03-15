f99d31025f46540322abd7f9cb413c96918f4416
Developstar <deyeye99@gmail.com>
2023-03-15 10:54:24 +0100

    added header component and router-view

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderNav/>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderNav from './components/HeaderNav.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderNav
   }
 }
 </script>
