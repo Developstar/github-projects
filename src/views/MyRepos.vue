2066a59cdd4fc031710fd30de476ef71e7eb88ce
Developstar <deyeye99@gmail.com>
2023-03-15 10:56:21 +0100

    created myRepository view

---
+++ src/views/MyRepos.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="pageWrapper">
+      <div>
+        <h1>Repo Page</h1>
+        <p>All my Repositories</p>
+      </div>
+    </section>
+  </template>
+  
+  <style>
+  .pageWrapper {
+    margin-top: 2rem;
+    text-align: center;
+  }
+  </style>
