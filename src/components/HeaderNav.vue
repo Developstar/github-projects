e5deedcee79e4195e32f4873d6c1023f8d254438
Developstar <deyeye99@gmail.com>
2023-03-16 07:02:42 +0100

    styled page links

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -4,7 +4,7 @@
       <router-link to="/">MyGit</router-link>
     </div>
     <div class="navigation">
-      <ul>
+      <ul class="pagelinks">
         <li>
           <router-link to="/">Home</router-link>
         </li>
@@ -16,4 +16,19 @@
     </div>
     </nav>
 </template>
+
+<style>
+
+.nav_wrapper{
+  background: blue;
+  display: flex;
+  gap: 50%;
+}
+
+.pagelinks{
+  display: flex;
+  gap: 5rem;
+}
+
+</style>
 
