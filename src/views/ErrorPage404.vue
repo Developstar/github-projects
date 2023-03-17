41f3f86221cd9a229dfbac9f55ace025cf1dca5a
Developstar <deyeye99@gmail.com>
2023-03-17 14:57:35 +0100

    created error page

---
+++ src/views/ErrorPage404.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Sorry this page does not exist</h1>
+        <router-link :to="{ name: 'HomePage' }">
+  Back to Home
+</router-link>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'ErrorPage404'
+}
+</script>
+
+<style>
+h1{
+    margin-top: 5rem;
+}
+</style>
