3925f6cbd5437c9a642025f26e1f63efb85b7c7a
Developstar <deyeye99@gmail.com>
2023-03-17 14:56:31 +0100

    imported Repodetails and Errorpage into Router

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import {createRouter, createWebHistory} from "vue-router"
 import HomePage from './views/HomePage.vue'
 import MyRepos from './views/MyRepos.vue'
+import RepoDetails from './views/RepoDetails.vue'
+import ErrorPage404 from '../src/views/ErrorPage404.vue'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -11,11 +13,30 @@ const router = createRouter({
         name: "HomePage",
         component: HomePage
     },
+
+    { path: '/:pathMatch(.*)*', component: ErrorPage404 },
+
     {
         path: "/MyRepos",
         name: "MyRepos",
-        component: MyRepos
-    }
+        component: MyRepos,
+        children: [
+            {
+                path: '/repo/:id',
+                name: 'RepoDetails',
+                component: RepoDetails,
+                props: (route) => ({ 
+                    id: Number(route.params.id),
+                    page: route.query.page || 1,
+                  }),
+              }
+      
+        ]
+        
+    },
+
+
+    
     
 ]
 
