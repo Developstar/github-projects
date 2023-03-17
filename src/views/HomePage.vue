85ea92d61067b9d93b1a77eab9812cf4c566e57a
Developstar <deyeye99@gmail.com>
2023-03-17 14:54:56 +0100

    added and styled  images and texts

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,9 +1,16 @@
 <template>
     <section class="pageWrapper">
-      <div>
-        <h1>Home Page</h1>
-        <p>This is Home Page</p>
+      <div class="imgWrapper"><img src="../assets/myImage.png" alt="my_img"/></div>
+      <div class="intro">
+      <div class="heading">
+        <h4>Adeyeye Boluwatife</h4>
+        <p>Frontend Developer</p>
       </div>
+      <div class="repoButton">
+        <button>My GithubRepos</button>
+      </div>
+      </div>
+
     </section>
   </template>
   
@@ -11,5 +18,34 @@
   .pageWrapper {
     margin-top: 2rem;
     text-align: center;
+    background: white;
+    margin-top: 10vh;
+    width: 80vw;
+    height: 70vh;
+    position: relative;
+  }
+
+  .imgWrapper{
+    position: absolute;
+    left: -2rem;
+    top: -2rem;
+    border-radius: 50%;
+    border: 4px solid #dbdde0;
+  }
+  .imgWrapper img{
+    width: 12rem;
+    height: 12rem;
+    border-radius: 50%;
+    background: white;
+
+  }
+
+  .intro{
+    display: flex;
+    align-items: center;
+  }
+
+  .heading, .repoButton{
+    margin-top: 2rem;
   }
   </style>
