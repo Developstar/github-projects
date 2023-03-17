f086f58807b2831eb11179f8066e06edf4446cc0
Developstar <deyeye99@gmail.com>
2023-03-17 14:57:11 +0100

    styled navigation links

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,16 +1,16 @@
 <template>
 <nav class="nav_wrapper">
     <div class="logo">
-      <router-link to="/">MyGit</router-link>
+      <router-link to="/" style="text-decoration: none;">MyGit</router-link>
     </div>
     <div class="navigation">
       <ul class="pagelinks">
         <li>
-          <router-link to="/">Home</router-link>
+          <router-link to="/" style="text-decoration: none;">Home</router-link>
         </li>
 
         <li>
-          <router-link to="/MyRepos">Repositories</router-link>
+          <router-link to="/MyRepos" style="text-decoration: none;">Repositories</router-link>
         </li>
       </ul>
     </div>
@@ -20,9 +20,17 @@
 <style>
 
 .nav_wrapper{
-  background: blue;
+  background: #ffffff;
   display: flex;
   gap: 50%;
+  height: 10vh;
+  box-shadow: 2px 2px 1px 1px rgba(0, 0, 0, 0.3);
+  align-items: center;
+}
+
+li{
+  text-decoration-style: none;
+  list-style: none;
 }
 
 .pagelinks{
