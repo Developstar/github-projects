1e93450ec869642fe9cd17dcadd0db02747237ad
Developstar <deyeye99@gmail.com>
2023-03-18 13:00:37 +0100

    added logic to get repo id from route params

--- src/views/RepoDetails.vue
+++ src/views/RepoDetails.vue
@@ -1,77 +1,42 @@
-<!-- <template>
-    <section class="pageWrapper">
-      <div>
-        <h1>A single Repo</h1>
-        <p></p>
-      </div>
-    </section>
-  </template>  -->
-
-  <template>
-    <div>
-      <h1>{{ repo.name }}</h1>
-      <p>{{ repo.description }}</p>
-      <p>Language: {{ repo.language }}</p>
-      <p>Stars: {{ repo.stargazers_count }}</p>
-    </div>
-  </template>
+<template>
+     
+     <div>
+    <h2>{{ repo.name }}</h2>
+    <p>{{ repo.description }}</p>
+    <p>{{ repo.language }}</p>
+    <p>{{ repo.stargazers_count }} stars</p>
+  </div>
+    
+</template>
   
   <script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      repo: {}
+    };
+  },
+  created() {
+    // This is to get the repo ID from the route params
+    const repoId = this.$route.params.repoId;
+    // This is to make an API call to get my repository details
+    axios.get(`https://api.github.com/repositories/${repoId}`)
+      .then(response => {
+        this.repo = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+}
 
-  // export default {
-  //   name: 'RepoDetails',
-  //   mounted() {
-  //   const id = this.$route.params.id
-    
-  // }
-  // }
+
   </script>
   
   
   <style>
-  /* .pageWrapper {
-    margin-top: 2rem;
-    text-align: center;
-  } */
+
   </style>
 
-  <!-- <template>
-    <div>
-      <h2>{{repo.name}}</h2>
-      <p>{{repo.description}}</p>
-      <p>Created: {{repo.created_at}}</p>
-      <p>Last updated: {{repo.updated_at}}</p>
-      <p>URL: <a :href="repo.html_url">{{repo.html_url}}</a></p>
-      <p>Stars: {{repo.stargazers_count}}</p>
-      <p>Forks: {{repo.forks_count}}</p>
-      <p>Language: {{repo.language}}</p>
-      <p>Issues: {{repo.open_issues_count}}</p>
-      <router-link :to="{ name: 'RepoIssues', params: { id: repo.id }}">View Issues</router-link>
-    </div>
-  </template>
   
-  <script>
-  import axios from 'axios';
-  export default {
-    data() {
-      return {
-        repo: null
-      };
-    },
-    created() {
-      const id = this.$route.params.id;
-      this.getRepoDetails(id);
-    },
-    methods: {
-      async getRepoDetails(repoId) {
-        try {
-          const response = await axios.get(`https://api.github.com/repositories/${repoId}`);
-          this.repo = response.data;
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    }
-  };
-  </script>
-   -->
