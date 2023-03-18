11dfbcf75431321673da9322509cfb54f4aeb920
Developstar <deyeye99@gmail.com>
2023-03-18 18:47:32 +0100

    styled repo details

--- src/views/RepoDetails.vue
+++ src/views/RepoDetails.vue
@@ -1,42 +1,70 @@
 <template>
-     
-     <div>
-    <h2>{{ repo.name }}</h2>
-    <p>{{ repo.description }}</p>
-    <p>{{ repo.language }}</p>
-    <p>{{ repo.stargazers_count }} stars</p>
-  </div>
-    
+  <section class="RepoDetailsWrapper">
+    <div class="textdiv">
+      <h1 style="text-align: center;">Repository Details</h1>
+      <p> <span class="title">Repository Name:</span> {{ repo.name }}</p>
+      <p> <span class="title">Repository Description:</span> {{ repo.description }}</p>
+      <p> <span class="title"></span>{{ repo.language }}</p>
+      <p> <span class="title">Repository Size:</span> {{ repo.size }} kb</p>
+      <p> <span class="title">Visibility:</span> {{ repo.visibility }}</p>
+      
+      <div class="repoDetailsbutton">
+        <router-link to="/MyRepos" style="text-decoration: none"
+          ><button class="mrepobtn backbtn">Back</button></router-link
+        >
+      </div>
+    </div>
+  </section>
 </template>
-  
-  <script>
-import axios from 'axios';
+
+<script>
+import axios from "axios";
 export default {
   data() {
     return {
-      repo: {}
+      repo: {},
     };
   },
   created() {
     // This is to get the repo ID from the route params
     const repoId = this.$route.params.repoId;
     // This is to make an API call to get my repository details
-    axios.get(`https://api.github.com/repositories/${repoId}`)
-      .then(response => {
+    axios
+      .get(`https://api.github.com/repositories/${repoId}`)
+      .then((response) => {
         this.repo = response.data;
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
-  }
+  },
+};
+</script>
+
+<style>
+.RepoDetailsWrapper {
+  border: 1px solid black;
+  background: white;
+  text-align: left;
+  width: 70%;
+  margin-top: 3rem;
+  height: 70vh;
+  box-shadow: 5px 5px 6px 3px rgba(0, 0, 0, 0.8);
 }
 
+.title{
+  font-size: 1.3rem;
+}
 
-  </script>
-  
-  
-  <style>
+.textdiv{
+  width: 80%;
+}
 
-  </style>
+.textdiv p{
+  margin-top: 1rem;
+}
 
-  
+.backbtn {
+  margin-top: 1rem;
+}
+</style>
