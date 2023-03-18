ec5690694167427f0e125d5d8f38d9db389a38a7
Developstar <deyeye99@gmail.com>
2023-03-18 18:41:57 +0100

    added about website details

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -7,18 +7,26 @@
         <p>Frontend Developer</p>
       </div>
       <div class="repoButton">
-        <button>My GithubRepos</button>
+        <router-link to="/MyRepos" style="text-decoration: none;"><button class="mrepobtn ">My Repos</button></router-link>
       </div>
+      </div>
+
+      <div class="aboutMe">
+        <h2>My Github Repositories</h2>
+        <p>This website showcases my GitHub details and projects in a visually pleasing manner. Discover my latest repositories, contributions, and activities, all in one place.</p>
       </div>
 
     </section>
   </template>
   
   <style>
+
+  
   .pageWrapper {
     margin-top: 2rem;
     text-align: center;
     background: white;
+    box-shadow: 5px 5px 6px 3px rgba(0, 0, 0, 0.8);
     margin-top: 10vh;
     width: 80vw;
     height: 70vh;
@@ -40,6 +48,8 @@
 
   }
 
+
+
   .intro{
     display: flex;
     align-items: center;
@@ -47,5 +57,18 @@
 
   .heading, .repoButton{
     margin-top: 2rem;
+  }
+
+  .aboutMe{
+    margin-top: 8rem;
+    width: 80%;
+    height: 20vh;
+    
+  }
+
+  .aboutMe h2{
+    margin-bottom: 1rem;
+    margin-top: 1rem;
+    
   }
   </style>
